4b46da4e4e141b9058bfce0ce1a45736a6050dd2
Saumya Singh <106618290+DestinyClover@users.noreply.github.com>
2023-09-10 20:18:27 +0530

    Delete P02_linear_search.py

--- P02_linear_search.py
+++
@@ -1,8 +0,0 @@
-lst=eval(input("enter list"))
-search= int(input("enter number to be searched"))
-for i in range(len(lst)):
-    if lst[i]==search:
-        print("position of given number is",i+1)
-        break
-else:
-    print("not found")
